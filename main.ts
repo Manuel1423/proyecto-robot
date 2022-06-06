b44328224012d0895867b1890160c961c3192141
Manuel1423 <106244068+Manuel1423@users.noreply.github.com>
2022-06-06 13:23:35 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+let a = 0
+let izq = 0
+let der = 0
 basic.forever(function () {
-	
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 40)
+    a = maqueen.readPatrol(maqueen.Patrol.PatrolLeft) + maqueen.readPatrol(maqueen.Patrol.PatrolRight)
+    izq = maqueen.readPatrol(maqueen.Patrol.PatrolLeft)
+    der = maqueen.readPatrol(maqueen.Patrol.PatrolRight)
+    if (izq) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 186)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 13)
+    }
+    if (der) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 186)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 13)
+    }
 })
